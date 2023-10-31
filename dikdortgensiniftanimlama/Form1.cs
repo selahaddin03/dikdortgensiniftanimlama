6121af37934e84f9c734233b42e4406d35a444d3
Selahaddin Doğan <149062529+selahaddin909@users.noreply.github.com>
2023-10-31 06:27:10 +0300

    Proje dosyası ekle.

---
+++ dikdortgensiniftanimlama/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dikdortgensiniftanimlama
+{
+
+    public partial class Form1 : Form
+    {
+        class Dikdortgen
+        {
+            private int a, b;
+            public Dikdortgen(int a, int b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+            public int AlanHesapla()
+            {
+                return a * b;
+            }
+            public int CevreHesapla()
+            {
+                return 2 * (a + b);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
